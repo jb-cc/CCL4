4c1d877f2aaf5fce080b97f37d3fde2545e2aeb2
KlemensMeyer <cc221011@fhstp.ac.at>
2024-06-16 17:38:17 +0200

    movement to Transform and Collarbones gone

--- Assets/KlemensAssets/ThirdPersonMovement.cs
+++ Assets/KlemensAssets/ThirdPersonMovement.cs
@@ -13,6 +13,13 @@ public class ThirdPersonMovement : MonoBehaviour
     [SerializeField] private float gravity = -9.81f;
     Vector3 downVelocity;
 
+    [SerializeField]
+    private float jumpingMaxHeight;
+    [SerializeField]
+    private float fallFactor = .9f;
+    private bool isOnGround = false;
+    private bool isJumping = false;
+
 
     private float turnSmoothTime = 0.1f;
     private float turnSmoothVelocity;
@@ -42,7 +49,8 @@ public class ThirdPersonMovement : MonoBehaviour
     {
         _player = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;
-        Physics.gravity = new Vector3(0f, gravity, 0f);
+        //Physics.gravity = new Vector3(0f, gravity, 0f);
+        StartCoroutine(FallControlFlow());
     }
 
     // Update is called once per frame
@@ -70,7 +78,8 @@ public class ThirdPersonMovement : MonoBehaviour
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             if(movementType == MovementType.TransformBased)
             {
-                transform.Translate(moveDirection.normalized * speed);
+                float strength = Vector3.Magnitude(moveDirection); 
+                transform.Translate(new Vector3(0f,0f,1f) * speed * strength);
             }else if(movementType == MovementType.PhysicsBased)
             {
                 _player.AddForce(moveDirection.normalized * speed, _forceMode);
@@ -88,18 +97,64 @@ public class ThirdPersonMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump") && isGrounded) //&& isGrounded
         {
             downVelocity.y = Mathf.Sqrt(jumpSpeed * -2f * gravity);
-            _player.AddForce(downVelocity * jumpSpeed, ForceMode.Impulse);
-            //_player.AddForce(Vector3.up * jumpSpeed, ForceMode.VelocityChange);
+            //_player.AddForce(downVelocity * jumpSpeed, ForceMode.Impulse);
+            //wrong _player.AddForce(Vector3.up * jumpSpeed, ForceMode.VelocityChange); wrong
+            StartCoroutine(JumpControlFlow());
+
+
 
         }
         if (!isGrounded)
         {
             downVelocity.y += gravity * Time.deltaTime;
-            _player.AddForce(downVelocity * Time.deltaTime, ForceMode.VelocityChange);
+            //_player.AddForce(downVelocity * Time.deltaTime, ForceMode.VelocityChange);
             
             //characterController.Move(downVelocity * Time.deltaTime);
         }
 
         
     }
+
+    private IEnumerator JumpControlFlow()
+    {
+        isJumping = true;
+        float jumpHeight = transform.position.y + jumpingMaxHeight;
+        _player.AddForce(Vector3.up * jumpSpeed, _forceMode);
+        while (transform.position.y < jumpHeight)
+        {
+        
+
+            yield return null;
+
+
+        }
+        _player.AddForce(Vector3.up * jumpSpeed * -1 * fallFactor * Time.deltaTime, _forceMode);
+        isJumping = false;
+    }
+    private IEnumerator FallControlFlow()
+    {
+        //RaycastHit hit;
+        //float prevY;
+        float currentY = transform.position.y;
+        while (true)
+        {
+            //bool raycastSuccess = Physics.Raycast(transform.position, transform.up * -1, out hit);
+            //if (raycastSuccess && hit.collider.gameObject.CompareTag("Ground") && hit.distance <= 0.50001f)
+            if(isGrounded)
+            {
+                isJumping = false;
+                StopCoroutine(JumpControlFlow());
+                isOnGround = true;
+            }
+            else
+            {
+                isJumping = true;
+
+                isOnGround = false;
+            }
+            yield return null;
+
+        }
+    }
+
 }

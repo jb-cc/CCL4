b8b7d22560d4a2d6d610c3831ca4cfe3e4143b18
Jonas Beer <115902941+jb-cc@users.noreply.github.com>
2024-06-29 15:51:13 +0200

    added mutebutton to pause menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 public class Pause : MonoBehaviour
 {
     private GameManager _gameManager;
+    [SerializeField] private GameObject muteButton;
+    [SerializeField] private GameObject unmuteButton;
 
     void Awake()
     {
@@ -30,6 +32,19 @@ public class Pause : MonoBehaviour
         _gameManager.ReturnToMainMenu();
     }
 
+    public void ToggleMute()
+    {
+        playToggle();
+        _gameManager.muted = !_gameManager.muted;
+        muteButton.SetActive(!_gameManager.muted);
+        unmuteButton.SetActive(_gameManager.muted);
+        Debug.Log("Muted: " + _gameManager.muted);
+    }
+    
+    public void playToggle()
+    {
+        _gameManager.playToggle();
+    }
 
     public void playClickSound()
     {
